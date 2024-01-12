3b9eda92625dc50b1a092ce55d2e53414d9dec84
Diego Vallejo-Huanga <diegovallejo1985@gmail.com>
2024-01-11 23:24:38 -0500

    deletign data

--- STRESS TEST/locustfile_website.py
+++
@@ -1,9 +0,0 @@
-from locust import HttpUser, task, between
-
-class MyUserWebsite(HttpUser):
-    wait_time = between(1, 5)
-    host = "http://ecuadoriansignlanguagefenedif.000webhostapp.com"
-
-    @task
-    def access_website(self):
-        self.client.get("/")
